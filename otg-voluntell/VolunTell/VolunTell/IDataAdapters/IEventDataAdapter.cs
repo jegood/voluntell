66620bd7538e4c9529e55cc82d14c1f3d18125e0
Angelo Carrabba <acarrabba6@gmail.com>
2017-07-27 22:51:43 -0400

    got rid of public

--- otg-voluntell/VolunTell/VolunTell/IDataAdapters/IEventDataAdapter.cs
+++ otg-voluntell/VolunTell/VolunTell/IDataAdapters/IEventDataAdapter.cs
@@ -11,17 +11,17 @@ namespace VolunTell.IDataAdapters
     {
 
         /// <summary>
-        /// returns list of volunteers
+        /// returns list of volunteers*
         /// </summary>
         /// <param name="eventId">Id of event</param>
-        public Task<List<Volunteer>> GetVolunteersForEventAsync(Guid eventId);
+        Task<List<Volunteer>> GetVolunteersForEventAsync(Guid eventId);
 
         /// <summary>
         /// Adds a new event and returns its respective guid
         /// </summary>
         /// <param name="Data">Event information</param>
         /// <returns>Guid for that form</returns>
-        public Task<Guid> AddEventAsync(Event Data);
+        Task<Guid> AddEventAsync(Event Data);
 
         
     }
